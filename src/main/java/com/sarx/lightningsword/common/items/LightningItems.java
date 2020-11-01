b1a2115bc0059917ad6c1f5282f93610f5f38d2d
sarxWorks <sarxworks@gmail.com>
2020-10-31 22:33:45 -0400

    Release Commit

--- src/main/java/com/sarx/lightningsword/common/items/LightningItems.java
+++ src/main/java/com/sarx/lightningsword/common/items/LightningItems.java
@@ -15,16 +15,16 @@ public class LightningItems {
 
 
     public static final Item lightningsword = register("lightningsword", new LSpecialSwordItem(LToolTier.LIGHTNING, 3, -2.4F, (new Item.Properties().group(CreativeTab.LightningSword))));
-    public static final Item lightningpickaxe = register("lightningpickaxe",new PickaxeItem(LToolTier.LIGHTNING,1,-2.8F,(new Item.Properties().group(CreativeTab.LightningSword))));
-    public static final Item lightningaxe = register("lightningaxe",  new AxeItem(LToolTier.LIGHTNING,2,-2.4F, (new Item.Properties().group(CreativeTab.LightningSword))));
-    public static final Item lightningshovel = register("lightningshovel", new ShovelItem(LToolTier.LIGHTNING,1,-2.4F,(new Item.Properties().group(CreativeTab.LightningSword))));
-    public static final Item lightninghoe = register("lightninghoe", new HoeItem(LToolTier.LIGHTNING,1,(new Item.Properties().group(CreativeTab.LightningSword))));
-    public static final Item lightningingot  = register("lightningingot", new Item(new Item.Properties().group(CreativeTab.LightningSword)));
+//    public static final Item lightningpickaxe = register("lightningpickaxe",new PickaxeItem(LToolTier.LIGHTNING,1,-2.8F,(new Item.Properties().group(CreativeTab.LightningSword))));
+//    public static final Item lightningaxe = register("lightningaxe",  new AxeItem(LToolTier.LIGHTNING,2,-2.4F, (new Item.Properties().group(CreativeTab.LightningSword))));
+//    public static final Item lightningshovel = register("lightningshovel", new ShovelItem(LToolTier.LIGHTNING,1,-2.4F,(new Item.Properties().group(CreativeTab.LightningSword))));
+//    public static final Item lightninghoe = register("lightninghoe", new HoeItem(LToolTier.LIGHTNING,1,(new Item.Properties().group(CreativeTab.LightningSword))));
+//    public static final Item lightningingot  = register("lightningingot", new Item(new Item.Properties().group(CreativeTab.LightningSword)));
 
-    public static final Item lightning_helmet = register("lightning_helmet", new ArmorItem(LArmorTier.LIGHTING, EquipmentSlotType.HEAD, (new Item.Properties().group(CreativeTab.LightningSword))));
-    public static final Item lightning_chestplate = register("lightning_chestplate", new ArmorItem(LArmorTier.LIGHTING, EquipmentSlotType.CHEST, (new Item.Properties().group(CreativeTab.LightningSword))));
-    public static final Item lightning_leggings = register("lightning_leggings", new ArmorItem(LArmorTier.LIGHTING, EquipmentSlotType.LEGS, (new Item.Properties().group(CreativeTab.LightningSword))));
-    public static final Item lightning_shoes = register("lightning_shoes", new ArmorItem(LArmorTier.LIGHTING, EquipmentSlotType.FEET, (new Item.Properties().group(CreativeTab.LightningSword))));
+//    public static final Item lightning_helmet = register("lightning_helmet", new ArmorItem(LArmorTier.LIGHTING, EquipmentSlotType.HEAD, (new Item.Properties().group(CreativeTab.LightningSword))));
+//    public static final Item lightning_chestplate = register("lightning_chestplate", new ArmorItem(LArmorTier.LIGHTING, EquipmentSlotType.CHEST, (new Item.Properties().group(CreativeTab.LightningSword))));
+//    public static final Item lightning_leggings = register("lightning_leggings", new ArmorItem(LArmorTier.LIGHTING, EquipmentSlotType.LEGS, (new Item.Properties().group(CreativeTab.LightningSword))));
+//    public static final Item lightning_shoes = register("lightning_shoes", new ArmorItem(LArmorTier.LIGHTING, EquipmentSlotType.FEET, (new Item.Properties().group(CreativeTab.LightningSword))));
 
     public static Item register(String name, Item item){
         ITEMS.register(name,()-> item);
